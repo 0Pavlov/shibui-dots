aef6f74332b4223ba9e88ca5067765aadef38a41
0Pavlov <ffsvttvscsvb@gmail.com>
2026-01-24 04:57:47 -0500

    Update nvim theme

--- nvim/.config/nvim/lua/current_theme.lua
+++ nvim/.config/nvim/lua/current_theme.lua
@@ -1,21 +1,21 @@
 require("noirbuddy").setup({
     colors = {
-        primary = "#dedede",   -- Your textPrimary
-        secondary = "#808080", -- Your textSecondary
-        background = "#121212", -- Your background
+        primary = "#dedede",   -- textPrimary
+        secondary = "#808080", -- textSecondary
+        background = "#121212", -- background
     },
     preset = "minimal",
 })
 vim.cmd.colorscheme("noirbuddy")
 
 -- Override the String color *after* the theme has loaded
--- This ensures your settings win against the preset.
+-- This ensures those settings win against the preset
 local Color = require("colorbuddy.color").Color
 local Group = require("colorbuddy.group").Group
 local c = require("colorbuddy.color").colors
 local s = require("colorbuddy.style").styles
 
--- Define your custom orange
+-- Define custom orange
 Color.new("MyOrange", "#ff9e64")
 
 -- Apply it to String and @string (TreeSitter)
