fc9b31a49f76a928bcc9fe39bd157df95761704e
0Pavlov <ffsvttvscsvb@gmail.com>
2026-09-24 16:21:42 -0400

    Add rivalcfg launch

--- hyprland/.config/hypr/hyprland/execs.lua
+++ hyprland/.config/hypr/hyprland/execs.lua
@@ -2,4 +2,5 @@
 hl.on("hyprland.start", function()
     hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme \"prefer-dark\"")
     hl.exec_cmd("awww-daemon")
+    hl.exec_cmd("rivalcfg -c #ef66fa --strip-top-color #ef66fa --strip-middle-color #ef66fa --strip-bottom-color #ef66fa")
 end)
